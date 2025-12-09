793d0c7099db0eb43172c6e2fe264b47a65bd8a6
Bandapalle <sourabh.v.bandapalle@accenture.com>
2025-12-09 19:16:17 +0530

    New Files added

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket        = "terraform-state-backend-new"
-    key           = "terraform.tfstate"
+    key           = "three-tier-demo/terraform.tfstate"
     region        = "us-east-1"
     use_lockfile  = true
     encrypt       = true
