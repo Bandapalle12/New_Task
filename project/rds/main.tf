908aabc82e2f2ff87798cf99e16cccb762f8bcee
Bandapalle <sourabh.v.bandapalle@accenture.com>
2025-12-09 13:25:11 +0530

    New Files added

--- project/rds/main.tf
+++ project/rds/main.tf
@@ -1,7 +1,11 @@
+data "aws_rds_engine_version" "mysql" {
+  engine = var.rds_engine
+}
+
 resource "aws_db_instance" "mysql" {
   identifier          = "three-tier-demo-db"
   engine              = "mysql"
-  engine_version      = data.aws_rds_engine_version.mysql.version   # FIXED
+  engine_version      = data.aws_rds_engine_version.mysql.version   
   instance_class      = "db.t3.micro"
   allocated_storage   = 20
   username            = var.rds_username
