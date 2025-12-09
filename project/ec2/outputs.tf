78ea7af8d2fd0dd83ece1fd9bf0f0c48b7da2daf
Bandapalle <sourabh.v.bandapalle@accenture.com>
2025-12-09 11:17:03 +0530

    New Files added

--- project/ec2/outputs.tf
+++ project/ec2/outputs.tf
@@ -1,9 +1,10 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.this[0].id
+  value       = aws_instance.ecs_instance[0].id
 }
 
 output "public_ip" {
   description = "Public IP of the EC2 instance"
-  value       = aws_instance.this[0].public_ip
+  value       = aws_instance.ecs_instance[0].public_ip
 }
+
