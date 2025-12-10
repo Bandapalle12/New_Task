338611042bd2fe5d490c40d6540aee4e5ae8c105
Bandapalle <sourabh.v.bandapalle@accenture.com>
2025-12-10 17:32:08 +0530

    New Files added

--- project/ec2/main.tf
+++ project/ec2/main.tf
@@ -16,6 +16,8 @@ resource "aws_instance" "ecs_instance" {
   
   associate_public_ip_address = true
 
+  iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.name
+
   user_data = <<-EOF
               #!/bin/bash
               echo ECS_CLUSTER=${var.project_name}-cluster >> /etc/ecs/ecs.config
@@ -24,4 +26,36 @@ resource "aws_instance" "ecs_instance" {
   tags = {
     Name = "${var.project_name}-ecs-instance"
   }
+
 }
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "${var.project_name}-ecs-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_ec2_ecr_policy" {
+  role       = aws_iam_role.ecs_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "${var.project_name}-ecs-instance-profile"
+  role = aws_iam_role.ecs_instance_role.name
+}
+
